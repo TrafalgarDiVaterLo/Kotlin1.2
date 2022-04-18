074a04d57f9d0ecd474fb89d1ad5ef9bd5010193
TrafalgarDiVaterLo <100771681+TrafalgarDiVaterLo@users.noreply.github.com>
2022-04-18 19:44:31 +0900

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,11 @@
 fun main() {
-    val totalPrice = 100_000
-    val discount = 100
-    val discount5 = 0.05
-    val discountMeloman = 0.01
-    val discountStart = 1_000
-    val discountEnd = 10_001
-    var regularСustomer: Boolean
+    val amount = 13436_00
+    val minCommission = 35
+    val commissionTransfer = 0.0075
 
+    val resultCommission = if (amount * commissionTransfer > minCommission) (amount * commissionTransfer) else minCommission
 
-    var result =
-        if (totalPrice > discountStart && totalPrice < discountEnd) totalPrice - discount else if (totalPrice > discountEnd) totalPrice - (totalPrice * discount5) else totalPrice
-    println("К оплате: $result руб.")
-    regularСustomer = true
-    var resultMeloman =
-        if (regularСustomer)result.toInt() - (result.toInt() * discountMeloman) else result
-    println("К оплате меломану: $resultMeloman руб.")
+    println("Комиссия: ${resultCommission.toInt()} коп.")
 }
 
 
