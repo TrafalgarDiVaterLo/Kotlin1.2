cc46fdfc9cc484c1efe25ef8ed35f8aadca3382d
Trafalgar Lo <Sanshez249704@bk.ru>
2022-04-18 15:19:05 +0900

    Money Transfer

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val amount = 13436_00
+    val minCommission = 35
+    val commissionTransfer = 0.0075
+
+    val resultCommission = if (amount * commissionTransfer > minCommission) (amount * commissionTransfer) else minCommission
+
+    println("Комиссия: ${resultCommission.toInt()} коп.")
+}
